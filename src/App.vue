5599b585f05672b19d07e905519cf85b45618946
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2024-06-22 13:45:40 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center vh-100">
-    <div class="row w-50">
-      <div class="col-sm-12 mb-3">
+  <div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="row w-5 w-md-50">
+      <div class="col-12 mb-3">
         <b-card class="text-center">
-          <b-dropdown id="dropdown-export" text="选择存档导出" variant="primary" class="m-md-2">
+          <b-dropdown id="dropdown-export" text="选择存档导出" variant="outline-primary">
             <template v-if="Object.keys(filteredLocalStorage).length">
-              <b-dropdown-item v-for="(value, key) in filteredLocalStorage" :key="key" @click="selectSave(key)">
+              <b-dropdown-item
+                v-for="(value, key) in filteredLocalStorage"
+                :key="key"
+                @click="selectSave(key)"
+              >
                 {{ key }}
               </b-dropdown-item>
             </template>
@@ -13,15 +17,23 @@
               <b-dropdown-item disabled>没有可以导出的存档</b-dropdown-item>
             </template>
           </b-dropdown>
-          <b-button variant="primary" :disabled="!selectedSaveKey" @click="exportSelectedSave">
+          <b-button
+            variant="outline-primary"
+            :disabled="!selectedSaveKey"
+            @click="exportSelectedSave"
+          >
             导出选中的存档
           </b-button>
         </b-card>
       </div>
-      <div class="col-sm-12 mb-3">
+      <div class="col-12 mb-3">
         <b-card class="text-center">
-          <b-button variant="primary" @click="triggerImport">导入存档</b-button>
+          <b-button variant="outline-primary" @click="triggerImport">导入存档</b-button>
           <input type="file" ref="importInput" @change="importLocalStorage" style="display: none" />
+        </b-card>
+      </div><div class="col-12 mb-3">
+        <b-card class="text-center">
+          <div>ps:导出存档记得导出RPG Global哦</div>
         </b-card>
       </div>
     </div>
@@ -30,7 +42,7 @@
 
 <script>
 import { BDropdown, BDropdownItem, BCard, BButton } from 'bootstrap-vue-next';
-import { selectSave, exportSelectedSave, importLocalStorage, triggerImport, loadLocalStorageItems } from './main';
+import { selectSave, exportSelectedSave, importLocalStorage, triggerImport, loadLocalStorageItems } from './sm';
 
 export default {
   name: 'App',
@@ -50,7 +62,7 @@ export default {
     filteredLocalStorage() {
       const filtered = {};
       for (const [key, value] of Object.entries(this.localStorageItems)) {
-        if (key.startsWith('RPG File ')) {
+        if (key.startsWith('RPG File') || key === 'RPG Global') {
           filtered[key] = value;
         }
       }
@@ -64,7 +76,7 @@ export default {
     triggerImport,
     loadLocalStorageItems
   },
-  mounted() {
+  async mounted() {
     this.loadLocalStorageItems();
     const getSystemTheme = () => window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
     const setTheme = (theme) => {
@@ -79,7 +91,12 @@ export default {
 </script>
 
 <style>
-.d-flex {
+.container {
   height: 100vh;
+  padding: 15px;
+}
+
+.card-body .dropdown {
+  padding: 5px;
 }
 </style>
